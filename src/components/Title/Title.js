80acb40e09042ac976f28bbe1ac3a8f55e207d79
feygia <feygia@elad.co.il>
2025-03-25 11:51:34 +0200

    dd

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     return (
         <div className='flex justify-end items-center pb-4 mb-6'>
             <h1 className='sm:text-3xl md:text-4xl lg:text-5xl text-[#002f87] font-semibold text-right font-noto'>
-                מערכת תמלול חכמה
+                מערכת תמלול חכמהה
             </h1>
         </div>
     )
